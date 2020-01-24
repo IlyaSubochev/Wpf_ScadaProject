4573ae076fab279addfce392f5bc137fc8f88e32
Subochev Ilya <siveternal@gmail.com>
2020-01-24 22:46:15 +0300

    Create test plc connection. Create Data for test.

---
+++ TestPLCConnection/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPLCConnection
+{
+    public class Data
+    {
+        public ushort W1 { get; set; }
+
+        public short DATE { get; set; }
+
+        public int TIME { get; set; }
+
+        public int DW { get; set; }
+
+        public double R1 { get; set; }
+
+        public double R2 { get; set; }
+
+        public double R3 { get; set; }
+
+        public ushort W2 { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
