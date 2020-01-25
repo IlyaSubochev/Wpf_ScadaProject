e6bebb481ce00292ff58c31aeab3aa5e30924744
Subochev Ilya <siveternal@gmail.com>
2020-01-25 22:47:02 +0300

    Start RaedBytes and MultiBytes

--- TestPLCConnection/Program.cs
+++ TestPLCConnection/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using S7.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,32 @@ namespace TestPLCConnection
     {
         static void Main(string[] args)
         {
+            Plc plc = new Plc(CpuType.S71500, "192.168.0.202", 0, 1);
+            if (plc.IsAvailable)
+            {
+                plc.Open();
+                if (!plc.IsConnected)
+                {
+                    //this reads first 200 bytes of DB1
+                    var bytes = plc.ReadBytes(DataType.DataBlock, 1, 0, 200);
+                }
+                else
+                    Console.WriteLine("Connection not alive");
+                plc.Close();
+            }
+            else
+                Console.WriteLine("PLC not available");
+            
+            
+        }
+
+        //ReadBytes it's limited to 200 bytes. Method with recusion
+        public static List<byte> ReadMultipleBytes(int numBytes, int db, int startByteAdr = 0)
+        {
+            List<byte> resultBytes = new List<byte>();
+
+
+            return resultBytes;
         }
     }
 }
