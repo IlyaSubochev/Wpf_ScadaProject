38ce01007b53f0ac7b980c2ccd2bb58906f597ef
Subochev Ilya <siveternal@gmail.com>
2020-01-30 22:47:42 +0300

    Create start up point with Bootstrapper

--- Wpf_ScadaProject/Views/RollingMillView.xaml.cs
+++ Wpf_ScadaProject/Views/RollingMillView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wpf_ScadaProject
+namespace Wpf_ScadaProject.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для RollingMillView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RollingMillView : Window
     {
-        public MainWindow()
+        public RollingMillView()
         {
             InitializeComponent();
         }
