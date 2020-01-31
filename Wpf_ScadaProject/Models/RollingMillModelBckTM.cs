898b8579c900bf5f96f182ffa97d76729217e907
Ilya Subochev <siveternal@gmail.com>
2020-01-31 15:27:31 +0300

    Add RollimgMillModel

---
+++ Wpf_ScadaProject/Models/RollingMillModelBckTM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_ScadaProject.Models
+{
+    public class RollingMillModelBckTM
+    {
+        public uint BackColorTM1 { get; set; }
+        public uint BackColorTM2 { get; set; }
+        public uint BackColorTM3 { get; set; }
+        public uint BackColorTM4 { get; set; }
+        public uint BackColorTM5 { get; set; }
+        public uint BackColorTM6 { get; set; }
+        public uint BackColorTM7 { get; set; }
+        public uint BackColorTM8 { get; set; }
+        public uint BackColorTM9 { get; set; }
+        public uint BackColorTM10 { get; set; }
+        public uint BackColorTM11 { get; set; }
+        public uint BackColorTM12 { get; set; }
+        public uint BackColorTM13 { get; set; }
+        public uint BackColorTM14 { get; set; }
+        public uint BackColorTM15 { get; set; }
+        public uint BackColorTM16 { get; set; }
+        public uint BackColorTM17 { get; set; }
+        public uint BackColorTM18 { get; set; }
+
+    }
+}
--- Wpf_ScadaProject/ViewModels/RollingMillViewModel.cs
+++ Wpf_ScadaProject/ViewModels/RollingMillViewModel.cs
@@ -1,13 +1,40 @@
 ﻿using Caliburn.Micro;
+using S7.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wpf_ScadaProject.Models;
 
 namespace Wpf_ScadaProject.ViewModels
 {
     public class RollingMillViewModel : Screen
     {
-    }
+		private int _testValue;
+      
+		public int TestValue
+		{
+			get 
+			{
+
+                return _testValue; 
+			}
+			set 
+			{ 
+				_testValue = value; 
+			}
+		}
+
+		private BindableCollection<RollingMillModelBckTM> _backColorTM = new BindableCollection<RollingMillModelBckTM>();
+
+		public BindableCollection<RollingMillModelBckTM> BackColorTM
+		{
+			get 
+			{
+				return _backColorTM;
+			}
+		}
+
+	}
 }
