343084d9b16134f91459fa2adeda4fbe22cb5bf9
Subochev Ilya <siveternal@gmail.com>
2020-02-01 22:46:00 +0300

    Create DispatcherTimer

--- Wpf_ScadaProject/ViewModels/RollingMillViewModel.cs
+++ Wpf_ScadaProject/ViewModels/RollingMillViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using Wpf_ScadaProject.Models;
 
 namespace Wpf_ScadaProject.ViewModels
@@ -12,8 +13,44 @@ namespace Wpf_ScadaProject.ViewModels
     public class RollingMillViewModel : Screen
     {
 		private int _testValue;
-      
-		public int TestValue
+		private DispatcherTimer timer=null;
+		private void timeStart()
+		{
+			timer = new DispatcherTimer();
+			timer.Tick += new EventHandler(timerTick);
+			timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+			timer.Start();
+		}
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            {
+                Plc plc = new Plc(CpuType.S71500, "192.168.0.202", 0, 1);
+                if (plc.IsAvailable)
+                {
+                    plc.Open();
+                    if (plc.IsConnected)
+                    {
+                        int count = 0;
+                        for (int i = 112; i < 2380; i += 126)
+                        {
+                            count += 1;
+                            var buffer = new RollingMillModelBckTM();
+                            plc.ReadClass(buffer, 9000, i);
+                        }
+                    }
+                    else
+                        Console.WriteLine("Connection not alive");
+                    plc.Close();
+                }
+                else
+                    Console.WriteLine("PLC not available");
+
+            }
+        }
+
+
+        public int TestValue
 		{
 			get 
 			{
