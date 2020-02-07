20196110a55830551525a1764e41dd5da78b9e1a
Ilya Subochev <siveternal@gmail.com>
2020-02-07 15:22:49 +0300

    Comment Dependency Property

--- Wpf_ScadaProject/Controls/RollingStandItem.xaml.cs
+++ Wpf_ScadaProject/Controls/RollingStandItem.xaml.cs
@@ -26,74 +26,74 @@ namespace Wpf_ScadaProject.Controls
             InitializeComponent();
         }
 
-        public Byte CurrentColor
-        {
-            get
-            {
-                return (Byte)GetValue(CurrentColorProperty);
-            }
-            set
-            {
-                SetValue(CurrentColorProperty, value);
-            }
-        }
+        //public Byte CurrentColor
+        //{
+        //    get
+        //    {
+        //        return (Byte)GetValue(CurrentColorProperty);
+        //    }
+        //    set
+        //    {
+        //        SetValue(CurrentColorProperty, value);
+        //    }
+        //}
 
-        public static readonly DependencyProperty CurrentColorProperty =
-            DependencyProperty.Register("CurrentColor", typeof(Byte), typeof(RollingStandItem),
-                new UIPropertyMetadata(0), new ValidateValueCallback(ValidateCurrentColor));
+        //public static readonly DependencyProperty CurrentColorProperty =
+        //    DependencyProperty.Register("CurrentColor", typeof(Byte), typeof(RollingStandItem),
+        //        new UIPropertyMetadata(0), new ValidateValueCallback(ValidateCurrentColor));
 
-        public static bool ValidateCurrentColor(object value)
-        {
-            if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 255)
-                return true;
-            else
-                return false;
-        }
+        //public static bool ValidateCurrentColor(object value)
+        //{
+        //    if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 255)
+        //        return true;
+        //    else
+        //        return false;
+        //}
 
-        void RollingStandItemColor(string name, Byte CurrentColor)
-        {
+        //void RollingStandItemColor(string name, Byte CurrentColor)
+        //{
             
-            object findEllipse = NameProperty;
-            if (findEllipse is Ellipse)
-            {
-                Ellipse currentEllipse = findEllipse as Ellipse;
+        //    object findEllipse = NameProperty;
+        //    if (findEllipse is Ellipse)
+        //    {
+        //        Ellipse currentEllipse = findEllipse as Ellipse;
 
-                switch (Convert.ToInt32(CurrentColor))
-                {
-                    case 0:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Gray);
-                        break;
-                    case 1:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Blue);
-                        break;
-                    case 2:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.AliceBlue);
-                        break;
-                    case 4:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Green);
-                        break;
-                    case 8:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Black);
-                        break;
-                    case 16:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.White);
-                        break;
-                    case 32:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Yellow);
-                        break;
-                    case 64:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Red);
-                        break;
-                    case 128:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.LightGray);
-                        break;
-                    default:
-                        currentEllipse.Fill = new SolidColorBrush(Colors.Gray);
-                        break;
-                }
-            }
-            
+        //        switch (Convert.ToInt32(CurrentColor))
+        //        {
+        //            case 0:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Gray);
+        //                break;
+        //            case 1:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Blue);
+        //                break;
+        //            case 2:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.AliceBlue);
+        //                break;
+        //            case 4:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Green);
+        //                break;
+        //            case 8:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Black);
+        //                break;
+        //            case 16:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.White);
+        //                break;
+        //            case 32:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Yellow);
+        //                break;
+        //            case 64:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Red);
+        //                break;
+        //            case 128:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.LightGray);
+        //                break;
+        //            default:
+        //                currentEllipse.Fill = new SolidColorBrush(Colors.Gray);
+        //                break;
+        //        }
+        //    }
 
-        }
+
+        //}
     }
 }
