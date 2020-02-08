40e842c790dce3c41b1c93386004c22fd3a3f788
Subochev Ilya <siveternal@gmail.com>
2020-02-08 23:05:33 +0300

    Preperead for set color

--- Wpf_ScadaProject/Views/RollingMillView.xaml.cs
+++ Wpf_ScadaProject/Views/RollingMillView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Wpf_ScadaProject.ViewModels;
 
 namespace Wpf_ScadaProject.Views
 {
@@ -19,15 +20,52 @@ namespace Wpf_ScadaProject.Views
     /// </summary>
     public partial class RollingMillView : Window
     {
+        public DependencyObject rollingMillView = new RollingMillView();
         public RollingMillView()
         {
             InitializeComponent();
-
+            RMViewModel = new RollingMillViewModel();
+            
             RollingStand01.upperRoll.Fill = new SolidColorBrush(Colors.Black);
             RollingStand01.lowerRoll.Fill = new SolidColorBrush(Colors.Red);
         }
-        
-        
+
+        public RollingMillViewModel RMViewModel { get; }
+
+        void Show()
+        {
+            foreach (var item in RMViewModel.BackColorTM)
+            {
+                
+            }
+        }
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        void FindElement()
+        {
+            foreach (UserControl itemUC in FindVisualChildren<UserControl>(rollingMillView))
+            {
+
+            }
+        }
 
     }
 }
